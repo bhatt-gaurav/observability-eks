57ad8791824342c1804b01f26d7f0db931edfb8a
Ubuntu <ubuntu@ip-172-31-25-136.us-west-2.compute.internal>
2024-12-03 14:43:10 +0000

    IAM roles added for cluster and worker nodes

--- locals.tf
+++ locals.tf
@@ -1,4 +1,10 @@
 locals {
   name_suffix = var.resource_tags["Name"]
   environment = var.resource_tags["Environment"]
+
+  #IAM
+  iam_cluster_role_name = var.iam_cluster_role_name != "" ? var.iam_cluster_role_name : null
+  iam_cluster_role_name_prefix = var.iam_cluster_role_name != "" ? null : local.name_suffix
+  iam_workers_role_name = var.iam_workers_role_name != "" ? var.iam_workers_role_name : null
+  iam_workers_role_name_prefix = var.iam_workers_role_name != "" ? null : local.name_suffix
 }
